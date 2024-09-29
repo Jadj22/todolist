97026191f9dfe07d992c76062f75b8a50d62a0ed
Jadj <jadjdev2@gmail.com>
2024-09-29 21:19:37 +0000

    Mise à jour de la fonction pour le bouton ajouter

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,27 +3,34 @@ import React from 'react';
 const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    if(task.id){
-        const date = new Date(task.id);
-        const updateTask = tasklist.map((todo) => (
-          todo.id === task.id ? { id: task.id, name:task.name, time: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}` } : todo
-        ));
-
-        setTasklist(updateTask); // Mise à jour de la liste des tâches
-        setTask({}); // Vide l'input apres la mise à jour
+  
+    // Vérifier si le champ task.name est vide
+    if (!task.name || task.name.trim() === "") {
+      alert("Le champ de saisie est vide. Veuillez entrer une tâche.");
+      return; // Ne pas soumettre si le champ est vide
+    }
+  
+    if (task.id) {
+      const date = new Date(task.id);
+      const updateTask = tasklist.map((todo) => (
+        todo.id === task.id ? { id: task.id, name: task.name, time: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}` } : todo
+      ));
+  
+      setTasklist(updateTask); // Mise à jour de la liste des tâches
+      setTask({}); // Vider l'input après la mise à jour
     } else {
       const date = new Date();
       const newTask = {
         id: date.getTime(),
         name: e.target.task.value,
         time: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-      }
-
-      setTasklist([...tasklist, newTask]);
-      setTask({}); // Vide l'input apres ajout de tache
+      };
+  
+      setTasklist([...tasklist, newTask]); // Ajouter la nouvelle tâche à la liste
+      setTask({}); // Vider l'input après ajout
     }
-  }
+  };
+  
 
   return (
     <section className="addTask">
